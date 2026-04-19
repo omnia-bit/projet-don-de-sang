6037be459ab19ca2c4e815f8ecd39135f2136037
Mariem938 <mariemfrikha1910@gmail.com>
2026-04-19 13:45:14 +0100

    Nettoyage de la structure et configuration Django

--- myproject/config/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def accueil(request):
-    return render(request, 'accueil.html')
--- myproject/donneurs/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # exemple
-    path('', views.index, name='donneurs_home'),
-]
