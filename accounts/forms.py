66b7ee558f7403b661301ceaa190486c129a363b
Lamis <lamisisswai@gmail.com>
2026-04-19 17:55:41 +0100

    ajout de fichier accounts forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,17 +1,32 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User, DonneurProfile, HopitalProfile
 
 
-class RegisterForm(UserCreationForm):
 
-    ROLE_CHOICES = (
-        ('donneur', 'Donneur'),
-        ('hopital', 'Hôpital'),
-    )
+# INSCRIPTION USER
 
-    role = forms.ChoiceField(choices=ROLE_CHOICES)
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    role = forms.ChoiceField(choices=User.ROLE_CHOICES)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'role', 'password1', 'password2']
+
+
+
+# PROFIL DONNEUR
+
+class DonneurProfileForm(forms.ModelForm):
+    class Meta:
+        model = DonneurProfile
+        fields = ['groupe_sanguin', 'sexe', 'date_naissance', 'ville', 'actif']
+
+
+# PROFIL HÔPITAL
+
+class HopitalProfileForm(forms.ModelForm):
+    class Meta:
+        model = HopitalProfile
+        fields = ['nom', 'adresse', 'ville', 'agrement', 'valide']
