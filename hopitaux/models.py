24bd48eae6442e46429d39713630f71ce0b80623
Lamis <lamisisswai@gmail.com>
2026-04-19 17:49:35 +0100

    Suppression de class profilhopital  deplacer vers accounts/models.py

--- hopitaux/models.py
+++ hopitaux/models.py
@@ -1,17 +1,7 @@
 from django.db import models
-from django.conf import settings
+from accounts.models import HopitalProfile
 
-
-class HopitalProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='hopital_profile')
-    nom_etablissement = models.CharField(max_length=200)
-    ville = models.CharField(max_length=100)
-    adresse = models.TextField()
-    telephone = models.CharField(max_length=15)
-    specialite = models.CharField(max_length=100, blank=True)
-
-    def __str__(self):
-        return self.nom_etablissement
+# DEMANDE DE SANG
 
 class DemandeSang(models.Model):
     GROUPE_CHOICES = (
@@ -21,28 +11,37 @@ class DemandeSang(models.Model):
         ('O+', 'O+'), ('O-', 'O-'),
     )
 
-    hopital = models.ForeignKey(HopitalProfile, on_delete=models.CASCADE, related_name='demandes')
+    hopital = models.ForeignKey(
+        HopitalProfile,
+        on_delete=models.CASCADE,
+        related_name='demandes'
+    )
+
     groupe_sanguin = models.CharField(max_length=5, choices=GROUPE_CHOICES)
-    quantite_necessaire = models.PositiveIntegerField(help_text="Nombre de poches ou volume")
+    quantite_necessaire = models.PositiveIntegerField()
+    description = models.TextField(blank=True)
     date_publication = models.DateTimeField(auto_now_add=True)
     est_resolue = models.BooleanField(default=False)
-    description = models.TextField(blank=True, help_text="Raison de l'urgence")
 
     def __str__(self):
-        status = "Résolue" if self.est_resolue else "En attente"
-        return f"Demande {self.groupe_sanguin} par {self.hopital.nom_etablissement} ({status})"
+        return f"{self.groupe_sanguin} - {self.hopital.nom}"
+
+
+
+# CAMPAGNE DE DON
 
 class CampagneCollecte(models.Model):
-    hopital = models.ForeignKey(HopitalProfile, on_delete=models.CASCADE, related_name='campagnes')
+    hopital = models.ForeignKey(
+        HopitalProfile,
+        on_delete=models.CASCADE,
+        related_name='campagnes'
+    )
+
     titre = models.CharField(max_length=200)
     description = models.TextField()
     lieu = models.CharField(max_length=200)
     date_debut = models.DateTimeField()
     date_fin = models.DateTimeField()
-    
-    def __cloturee__(self):
-        from django.utils import timezone
-        return timezone.now() > self.date_fin
 
     def __str__(self):
-        return f"{self.titre} - {self.hopital.nom_etablissement}"
+        return f"{self.titre} - {self.hopital.nom}"
