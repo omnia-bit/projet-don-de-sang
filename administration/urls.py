e173d1579d140ba78f2c84b01247ea2dc6d24cdc
Mariem938 <mariemfrikha1910@gmail.com>
2026-05-09 22:03:07 +0100

    Modernisation et dynamisation complète de l'interface Admin (Cartes, Alertes, Recherche)

--- administration/urls.py
+++ administration/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('validation/', views.valider_hopitaux, name='validation_hopitaux'),
     path('validation/<int:pk>/<str:action>/', views.action_validation_hopital, name='action_validation'),
     path('export/donneurs/', views.export_donneurs_csv, name='export_donneurs'),
+    path('demandes/', views.liste_demandes_urgentes, name='liste_demandes'),
 ]
